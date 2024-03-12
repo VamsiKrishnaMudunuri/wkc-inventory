daac5e0731363e1bbfeee1a5fd0fd05fec69b74e
VamsiKrishnaMudunuri <vamsikrishna.mudunuri@gmail.com>
2024-03-12 09:26:59 -0400

    fix

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Exception;
 use Session;
 
-class userController extends Controller
+class UserController extends Controller
 {
     public function welcome(Request $request)
     {
